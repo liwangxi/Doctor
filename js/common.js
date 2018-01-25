26d2b8d7dafbf947f196b213aa9a2012430133ae
李望喜 <1216239264@qq.com>
2018-01-25 14:58:55 +0800

    第2种版式

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(".foot-head span").click(function(){
     $('html , body').animate({ scrollTop: 0 }, 'slow');
 })
+function right() {
+    let left = ($(window).width()-1150)/2
+    $(".content-right").css("right",left);
+}
+$(document).ready(function(){
+    right()
+})
+$(document).resize(() => {
+    right()
+})
